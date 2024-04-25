367d2db0db344794290d650d3a6ec228b836f1e8
konstantinByr <167339543+konstantinByr@users.noreply.github.com>
2024-04-25 10:15:17 +0200

    Final

--- model.py
+++ model.py
@@ -7,15 +7,14 @@ class NeuralNet(nn.Module):
         self.layer2 = nn.Linear(hiddenSize, hiddenSize)
         self.layer3 = nn.Linear(hiddenSize, hiddenSize)
         self.relu = nn.ReLU()
-        # Initialisiere die activations-Liste für jede Schicht
-        self.activations = [[] for _ in range(3)]  # Hier 3 für 3 Schichten
+        self.activations = [[] for _ in range(3)]  # Liste zum Speichern der Aktivierungen für jede Schicht
 
     def forward(self, x):
         out = self.layer1(x)
-        self.activations[0].append(out)  # Speichere die Aktivierung der ersten Schicht
+        self.activations[0].append(out.clone().detach())  # Speichern der Aktivierung der ersten Schicht
         out = self.layer2(out)
-        self.activations[1].append(out)  # Speichere die Aktivierung der zweiten Schicht
+        self.activations[1].append(out.clone().detach())  # Speichern der Aktivierung der zweiten Schicht
         out = self.relu(out)
         out = self.layer3(out)
-        self.activations[2].append(out)  # Speichere die Aktivierung der dritten Schicht
+        self.activations[2].append(out.clone().detach())  # Speichern der Aktivierung der dritten Schicht
         return out
